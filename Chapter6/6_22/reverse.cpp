6b5705434bc8197830000e61654f9bf91827de08
Sliver_0812 <116070589+PwwwLe@users.noreply.github.com>
2023-05-13 11:46:45 +0800

    update 6_22
    update 6_22

--- Chapter6/6_22/reverse.cpp
+++ Chapter6/6_22/reverse.cpp
@@ -9,17 +9,20 @@ using namespace std;
 
 string reverse(string &str) {
     int i = 0, j = str.length() - 1;
-    string newStr;
-    while (i < j){
-        newStr = str[j] + newStr + str[i];
+    while (i <= j) {
+        char temp;
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        i++, j--;
     }
-    return newStr;
+    return str;
 }
 
-int main (){
+int main() {
     cout << "Please enter the original string:" << endl;
     string str;
     cin >> str;
-    cout <<"The reverse of it is :" << reverse(str) << endl;
+    cout << "The reverse of it is :" << reverse(str) << endl;
     return 0;
 }
